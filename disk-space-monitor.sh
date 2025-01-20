91abf0520e9da357824e0ef8697ed8e182eec636
Manjunath <manjunath809511@gmail.com>
2025-01-20 15:16:54 +0530

    Added disk space monitor script

---
+++ disk-space-monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+log_file="$HOME/logs1/disk_usage_monitor.log"
+
+threshold=10
+
+df -h | awk 'NR>1 {print $5 " " $1}' | while read usage partition; do
+    usage_value=${usage%\%}
+
+    if [ "$usage_value" -gt "$threshold" ]; then
+        echo "$(date) - Warning: disk usage on $partition is at $usage" >> "$log_file"
+        echo "Warning: disk usage on $partition is at $usage"
+    fi
+done
